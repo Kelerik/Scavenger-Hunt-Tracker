81903894cf3ce191f2c604cf393635973a5a228d
Kelerik <vincent.ng39@gmail.com>
2021-11-15 18:41:12 -0500

    more drag and drop functionality testing

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,46 @@ function makeDraggable(objectParam) {
         zIndex: 100,
         revert: "invalid",
         revertDuration: 100,
+        start: function (event, ui) {
+            console.log(ui.helper);
+
+            $(".card").each(function () {
+                // only highlight the next cards, not the current or previous ones
+                if (
+                    $(this).attr("id").replace("card-", "") >
+                    objectParam.closest(".card").attr("id").replace("card-", "")
+                ) {
+                    $(this).toggleClass("border-light border-success");
+                }
+            });
+        },
+        stop: function () {
+            $(".card").removeClass("border-success");
+            $(".card").addClass("border-light");
+        },
     });
 }
+
+// clone the first list and apply unique IDs
+for (let i = 0; i < 7; i++) {
+    var cloneList = $(".col").last().clone();
+    cloneList.find("li").remove();
+    cloneList.appendTo(".row");
+}
+$(".col").each(function (i) {
+    $(this)
+        .find(".card")
+        .attr("id", "card-" + i);
+    $(this)
+        .find(".accordion-collapse")
+        .attr("id", "accordion-panel-" + i);
+    $(this)
+        .find("button")
+        .attr("data-bs-target", "#accordion-panel-" + i);
+    $(this)
+        .find("button")
+        .append(" " + (i + 1));
+});
 
 // make droppable targets for the dragged elements
 $(".card").droppable({
@@ -19,8 +57,7 @@ $(".card").droppable({
         makeDraggable(newListItem);
         // make original item no longer draggable
         ui.draggable.draggable("disable");
-        ui.draggable.addClass("bg-success bg-gradient text-light");
+        ui.draggable.addClass("bg-success text-light");
     },
 });
-
 makeDraggable($(".list-group-item"));
