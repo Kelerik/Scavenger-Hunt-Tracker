e9113ab53e195413135080fff5e847147eb4e7eb
Kelerik <vincent.ng39@gmail.com>
2021-11-15 20:35:39 -0500

    items can only drag to next list

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,18 @@
 // make an object draggable
+// we assume this function is only ever used on list items inside the cards
 function makeDraggable(objectParam) {
     objectParam.draggable({
         helper: "clone",
         zIndex: 100,
         revert: "invalid",
         revertDuration: 100,
-        start: function (event, ui) {
-            console.log(ui.helper);
-
-            $(".card").each(function () {
-                // only highlight the next cards, not the current or previous ones
-                if (
-                    $(this).attr("id").replace("card-", "") >
-                    objectParam.closest(".card").attr("id").replace("card-", "")
-                ) {
-                    $(this).toggleClass("border-light border-success");
-                }
-            });
+        start: function () {
+            // get the number of the next location
+            var nextLocation = parseInt($(this).attr("data-location")) + 1;
+            // then highlight it
+            $(".card[data-location='" + nextLocation + "']").toggleClass(
+                "border-light border-success"
+            );
         },
         stop: function () {
             $(".card").removeClass("border-success");
@@ -25,10 +21,12 @@ function makeDraggable(objectParam) {
     });
 }
 
-// clone the first list and apply unique IDs
-for (let i = 0; i < 7; i++) {
+// for testing
+// clone the first list and apply unique IDs and data-location attributes
+for (let i = 1; i < 7; i++) {
     var cloneList = $(".col").last().clone();
     cloneList.find("li").remove();
+    cloneList.find(".card").attr("data-location", i);
     cloneList.appendTo(".row");
 }
 $(".col").each(function (i) {
@@ -46,18 +44,25 @@ $(".col").each(function (i) {
         .append(" " + (i + 1));
 });
 
-// make droppable targets for the dragged elements
-$(".card").droppable({
-    accept: ".list-group-item",
-    drop: function (event, ui) {
-        // when item is dropped, append a copy of it to the list
-        var newListItem = ui.draggable.clone();
-        $(this).find("ol").append(newListItem);
-        // make that new item draggable
-        makeDraggable(newListItem);
-        // make original item no longer draggable
-        ui.draggable.draggable("disable");
-        ui.draggable.addClass("bg-success text-light");
-    },
+// make each card a droppable target for the dragged elements
+$(".card").each(function (i) {
+    $(this).droppable({
+        // only accept items from the previous list
+        accept: ".list-group-item[data-location='" + (i - 1) + "']",
+        drop: function (event, ui) {
+            // when item is dropped, append a copy of it to the list
+            var newListItem = ui.draggable.clone();
+            // assign new data-location number
+            newListItem.attr("data-location", i);
+            // append item to the list
+            $(this).find("ol").append(newListItem);
+            // make that new item draggable
+            makeDraggable(newListItem);
+            // make original item no longer draggable
+            ui.draggable.draggable("disable");
+            ui.draggable.addClass("bg-success text-light");
+        },
+    });
 });
+
 makeDraggable($(".list-group-item"));
